81fd8130bff6c0fb80ca332151fccc8d85f27bb7
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2019-07-08 17:14:59 +0000

    prueba

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -1,20 +1,16 @@
-"""
-WSGI config for webapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/jatobarv/webapp'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'webapp.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
-
 application = get_wsgi_application()
-
-
-application = DjangoWhiteNoise(application)
